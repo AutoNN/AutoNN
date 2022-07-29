3145bc1224f43dfd1e1e678f73d9fe6cccfea246
sagniknayak <sagniknayak26@gmail.com>
2022-07-29 13:32:05 +0530

    updating

--- preprocessing/data_cleaning.py
+++ preprocessing/data_cleaning.py
@@ -1,2 +1,11 @@
 from dask import dataframe as dd
 from sklearn import pipeline
+
+class DataCleaning:
+
+    def __init__(self, dataframe: dd, label: str) -> None:
+        self.__dataset = dataframe
+        self.__label = label
+
+
+    def __ordinality()
