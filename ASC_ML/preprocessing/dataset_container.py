aacffed5b806a61999bb109bc130f241a887482f
sagniknayak <sagniknayak26@gmail.com>
2022-08-14 10:39:43 +0530

    updating preprocessing (still under construction)

--- ASC_ML/preprocessing/dataset_container.py
+++ ASC_ML/preprocessing/dataset_container.py
@@ -4,29 +4,35 @@ from column_info import ColumnInfo
 
 class DatasetContainer:
     def __init__(self, label: str, train_dataframe: dd,validation_dataframe = None, test_dataframe = None, override = False) -> None:
-        self.__train_dataset = train_dataframe
-        self.__validation_dataset = validation_dataframe
-        self.__test_dataset = test_dataframe
+        self.__dataset = dict()
+        self.__dataset['train'] = train_dataframe
+        self.__dataset['validation'] = validation_dataframe
+        self.__dataset['test'] = test_dataframe
         self.__label = label
         self.__override = override
 
     def __is_override(self) -> bool:
         return self.__override
 
-    def get_columns(self):
-        return self.__train_dataset.columns
+    def get_columns(self, types = ['train']):
+        dataset = list()
+        for type in types:
+            dataset.append(self.__dataset[type])
+        return dataset
 
     def get_label(self) -> str:
         return self.__label
 
+    def set(self, dataset: dd, type = 'train'):
+        self.__dataset[type] = dataset
 
     def get(self) -> dd:
-        return self.__train_dataset, self.__validation_dataset, self.__test_dataset
+        return self.__dataset
 
 
     def train_test_split(self, test_split = 0.2, validation_split = None, shuffle = True, random_state = None) -> None:
-        if self.__is_override or self.__test_dataset == None:
-            self.__train_dataset, self.__test_dataset = train_test_split(self.__train_dataset, test_size=test_split, shuffle=shuffle, random_state=random_state)
+        if self.__is_override or self.__dataset['test'] == None:
+            self.__dataset['train'], self.__dataset['test'] = train_test_split(self.__dataset['train'], test_size=test_split, shuffle=shuffle, random_state=random_state)
         else:
             print("Test Dataset already exists")
             print("No changes are done to the Test Dataset")
@@ -35,8 +41,8 @@ class DatasetContainer:
             self.train_validation_split(validation_split*((1-test_split)**-1), shuffle, random_state)
 
     def train_validation_split(self, validation_split = 0.1, shuffle = True, random_state = None) -> None:
-        if self.__is_override or self.__validation_dataset == None:
-            self.__train_dataset, self.__validation_dataset = train_test_split(self.__train_dataset, test_size=validation_split, shuffle=shuffle, random_state=random_state)
+        if self.__is_override or self.__dataset['validation'] == None:
+            self.__dataset['train'], self.__dataset['validation'] = train_test_split(self.__dataset['train'], test_size=validation_split, shuffle=shuffle, random_state=random_state)
         else:
             print("Validation Dataset already exists")
             print("No changes are done to the Validation Dataset")
--- ASC_ML/preprocessing/nan_handling.py
+++ ASC_ML/preprocessing/nan_handling.py
@@ -1,3 +1,12 @@
+from dataset_container import DatasetContainer as dc
+
 class missing_data_management:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, dataset: dc) -> None:
+        self.__dataset_bucket = dataset
+
+    
+    def remove_row_nan(self):
+        types = ['train','valid','test']
+        for type in types:
+            self.__dataset_bucket.set(self.__dataset_bucket.get(types = [type]).dropna(how='all', subset=self.__dataset_bucket.get_label()))
+
