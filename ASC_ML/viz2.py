6a3c535d649ba8d81b6b423242a9f8870fef7f41
Arjun Ghosh <gharjun69@gmail.com>
2022-08-15 14:14:24 +0530

    Update viz2.py

--- ASC_ML/viz2.py
+++ ASC_ML/viz2.py
@@ -2,6 +2,14 @@ from autoviz.AutoViz_Class import AutoViz_Class
 %matplotlib inline
 AV = AutoViz_Class()
 
-filename = "" #provide filename
-
-viz = AV.AutoViz(filename, chart_format="html")
+filename = 'E:\Seminar Conferences\CDAC\AUTO ML\AutoViz-master\car_design.csv'
+class dataviz:
+    def __init__(self, name):
+        self.name = name
+    
+    # Viz Method
+    def autoviz(self):
+        viz = AV.AutoViz(self.name, depVar = "price", chart_format="html") 
+ 
+p = dataviz(filename)
+p.autoviz()
