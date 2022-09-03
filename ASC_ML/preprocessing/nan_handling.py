c3c6cf68d8cae716653bc1026d5039012cb974b0
sagniknayak <sagniknayak26@gmail.com>
2022-09-03 20:13:05 +0530

    adding nan handling

--- ASC_ML/preprocessing/nan_handling.py
+++ ASC_ML/preprocessing/nan_handling.py
@@ -1,12 +1,28 @@
 from dataset_container import DatasetContainer as dc
 
-class missing_data_management:
-    def __init__(self, dataset: dc) -> None:
-        self.__dataset_bucket = dataset
+class DataHandling:
+    def __init__(self, dataset: dc, col_inf: dict, missing_threshold = 0.2) -> None:
+        self.__bucket = dataset
+        self.__column_info = col_inf
+        self.__dropped = list()
+        self.__full = list()
+        self.__unfilled = list()
+        self.__label = list()
+        self.__threshold = missing_threshold
+        self.__determine_type()
+        self.__allocate()
 
-    
-    def remove_row_nan(self):
-        types = ['train','valid','test']
-        for type in types:
-            self.__dataset_bucket.set(self.__dataset_bucket.get(types = [type]).dropna(how='all', subset=self.__dataset_bucket.get_label()))
+    def __determine_type(self):
+        for col in self.__column_info.keys():
+            if self.__column_info[col]['is_label']:
+                self.__label.append(col)
+            else:
+                if self.__column_info[col]['missing'] == 0:
+                    self.__full.append(col)
+                elif self.__column_info[col]['missing'] > self.__threshold:
+                    self.__dropped.append(col)
+                else:
+                    self.__unfilled.append(col)
 
+    def __allocate(self):
+        pass
