6ec1d8d967ec138a2e28712d06fddce0379b3a6d
Rajarshi Banerjee <rajarshiban13@gmail.com>
2022-12-06 22:43:09 +0530

    manifest ++

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ readme = currect_file.parent/"README.md"
 
 setup(
     name = "nocode-autonn",
-    version = "2.3.2",
+    version = "2.3.3",
     author_email="raju.banerjee.720@gmail.com",
     description="An AutoML framework for deep learning",
     long_description=readme.read_text(),
